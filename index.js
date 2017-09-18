e9417c634df5587baee42679837db75134bf2ea7
Aik Chun <aikchun616@gmail.com>
2017-09-18 14:41:17 +0800

    Created a sample Electron app that sends and receives data to and from the mainWindow and Electron app

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-    new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
+
+ipcMain.on('video:submit', (event, path) => {
+    mainWindow.webContents.send('video:metadata', 'Coming back from electron.');
+})
